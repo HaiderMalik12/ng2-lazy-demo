d44126d3d7f96462c436ec0b0b275621f0bd4b02
HaiderMalik <haidermalik504@gmail.com>
2017-04-18 06:46:15 +0500

    Fixed promise-polifill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,17 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
 
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyBdQmuJ7lwRsc0WrfvbVBouTSVkgV-9Wzw",
+    authDomain: "lazy-demi.firebaseapp.com",
+    databaseURL: "https://lazy-demi.firebaseio.com",
+    projectId: "lazy-demi",
+    storageBucket: "lazy-demi.appspot.com",
+    messagingSenderId: "848331258275"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import {AppRoutingModule} from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
